909ead2d4aa6691dc742cfe916dc2caa818523ba
wissalbn <wissal.benali88@gmail.com>
2023-12-13 14:49:37 +0100

    first

---
+++ src/UserInterface.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class UserInterface extends Frame {
+    public UserInterface(String title){
+        super(title);
+        setBounds(200,200,300,400);
+        Button b=new Button("clic");
+        TextArea t=new TextArea();
+        
+    }
+}
